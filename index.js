a704e136f7622fa586f17998c7b0c531d7da9719
Jonathan Nicolas <jonathan.s.nicolas@gmail.com>
2019-02-19 15:38:41 +0700

    Add get users endpoint

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const app = express()
 
 const port = 8000
 
+const users = [
+  {
+    id: 1,
+    name: 'Toto',
+  },
+  {
+    id: 2,
+    name: 'Toto2',
+  },
+  {
+    id: 3,
+    name: 'Toto3',
+  },
+]
+
 app.get('/', (req, res) => {
   res.send({ message: 'Hello World' })
+})
+
+app.get('/users', (req, res) => {
+  res.send(users)
 })
 
 app.listen(port, () => {
